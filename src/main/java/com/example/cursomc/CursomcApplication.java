9b901228c11ad8e11415dbfd1bb5dd1c41033dbf
Mario Andre <mario.andre.franco@gmail.com>
2018-07-30 20:35:59 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/example/cursomc/CursomcApplication.java
+++ src/main/java/com/example/cursomc/CursomcApplication.java
@@ -8,27 +8,42 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.domain.Produto;
 import com.example.cursomc.repositories.CategoriaRepository;
+import com.example.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
+	private CategoriaRepository categoriaRepository; // criação de dependencias
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		Categoria cat1 = new Categoria (null, "Informática");
-		Categoria cat2 = new Categoria(null,"Escritório");
-		
-		categoriaRepository.save(Arrays.asList(cat1, cat2));
-		
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 50.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3)); // quais produtos estao associados as categorias
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		categoriaRepository.save(Arrays.asList(cat1, cat2)); //salva todas categorias
+		produtoRepository.save(Arrays.asList(p1, p2, p3)); //salva todos os produtos
+
 	}
-	
-	
+
 }

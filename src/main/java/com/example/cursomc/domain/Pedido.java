1d63e74480f2d95a405f699557fed60fe370f294
Mario André Carvalho <mario.andre.franco@gmail.com>
2018-08-02 19:44:10 -0300

    EndPoint/pedidos

--- src/main/java/com/example/cursomc/domain/Pedido.java
+++ src/main/java/com/example/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.example.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,7 +12,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable {
@@ -20,12 +26,16 @@ public class Pedido implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
 	private Date instante;
 	
+	
+	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL,mappedBy="pedido")//pra evitar erro 
 	private Pagamento pagamento;
 	
-	
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id") 
 	private Cliente cliente;
@@ -33,6 +43,11 @@ public class Pedido implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+	
+	
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> items = new HashSet<>();
+	
 	
 	public Pedido () {
 		
@@ -110,6 +125,14 @@ public class Pedido implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public Set<ItemPedido> getItems() {
+		return items;
+	}
+
+	public void setItems(Set<ItemPedido> items) {
+		this.items = items;
 	}
 	
 }
--- src/main/java/com/example/cursomc/domain/Produto.java
+++ src/main/java/com/example/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.example.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,8 +13,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
@@ -31,6 +35,10 @@ public class Produto implements Serializable {
 																																					// tabelas
 	private List<Categoria> categorias = new ArrayList<>();
 
+	@JsonIgnore
+	@OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> items = new HashSet<>();
+
 	public Produto() {
 
 	}
@@ -42,6 +50,18 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 
+	
+	@JsonIgnore
+	public List<Pedido> getPedidos() {
+	List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : items) {
+			lista.add(x.getPedido());
+		}
+		
+		return lista;
+	
+	
+	}
 	public Integer getId() {
 		return id;
 	}
@@ -97,6 +117,14 @@ public class Produto implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public Set<ItemPedido> getItems() {
+		return items;
+	}
+
+	public void setItems(Set<ItemPedido> items) {
+		this.items = items;
 	}
 
 }
